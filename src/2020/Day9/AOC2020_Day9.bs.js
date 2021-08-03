321d3e43ddb1f8b5e21059c3bade50774a199917
Danny Siu <danny.siu@gmail.com>
2021-08-03 18:44:04 +0800

    2020 Day 9 - Day 1 in progress - sum matching works

--- src/2020/Day9/AOC2020_Day9.bs.js
+++ src/2020/Day9/AOC2020_Day9.bs.js
@@ -4,6 +4,7 @@
 var Belt_Int = require("rescript/lib/js/belt_Int.js");
 var Belt_Array = require("rescript/lib/js/belt_Array.js");
 var Belt_Option = require("rescript/lib/js/belt_Option.js");
+var Belt_Result = require("rescript/lib/js/belt_Result.js");
 var Utils$AdventOfCode = require("../../Utils.bs.js");
 
 function codes(t) {
@@ -14,28 +15,63 @@ function runLength(t) {
   return t.runLength;
 }
 
-function isSumOf(xs, x) {
-  return true;
+function isSumofWith(xs, x, sum) {
+  return Belt_Array.keep(xs, (function (a) {
+                if (a !== x) {
+                  return (a + x | 0) === sum;
+                } else {
+                  return false;
+                }
+              }));
+}
+
+function isSumOf(xs, sum) {
+  return Belt_Array.keep(xs, (function (a) {
+                return isSumofWith(xs, a, sum).length !== 0;
+              }));
 }
 
 function isCodeValid(t, i) {
   var lastSet = Belt_Array.slice(t.codes, i - t.runLength | 0, t.runLength);
-  var code = Belt_Array.getExn(t.codes, i);
-  Utils$AdventOfCode.log(code);
-  Utils$AdventOfCode.log(lastSet);
-  return true;
+  var c = Belt_Array.get(t.codes, i);
+  if (c !== undefined) {
+    Utils$AdventOfCode.log(c);
+    Utils$AdventOfCode.log(lastSet);
+    return {
+            TAG: /* Ok */0,
+            _0: isSumOf(lastSet, c)
+          };
+  } else {
+    return {
+            TAG: /* Error */1,
+            _0: /* InvalidIndex */1
+          };
+  }
 }
 
 function make(codes, runLength) {
-  return {
-          codes: codes,
-          runLength: runLength
-        };
+  Utils$AdventOfCode.log(runLength);
+  Utils$AdventOfCode.log(codes.length);
+  if (codes.length > runLength) {
+    return {
+            TAG: /* Ok */0,
+            _0: {
+              codes: codes,
+              runLength: runLength
+            }
+          };
+  } else {
+    return {
+            TAG: /* Error */1,
+            _0: /* InvalidRunLength */0
+          };
+  }
 }
 
 var Xmax = {
   codes: codes,
   runLength: runLength,
+  isSumofWith: isSumofWith,
   isSumOf: isSumOf,
   isCodeValid: isCodeValid,
   make: make
@@ -48,13 +84,9 @@ function parse(data) {
 }
 
 function solvePart1(data) {
-  var xmax_codes = parse(data);
-  var xmax = {
-    codes: xmax_codes,
-    runLength: 5
-  };
-  Utils$AdventOfCode.log(xmax);
-  return isCodeValid(xmax, 5);
+  var xmax = make(parse(data), 5);
+  Utils$AdventOfCode.log(Belt_Result.getExn(xmax));
+  return Belt_Result.getExn(isCodeValid(Belt_Result.getExn(xmax), 14));
 }
 
 function solvePart2(data) {
--- src/2020/Day9/AOC2020_Day9.res
+++ src/2020/Day9/AOC2020_Day9.res
@@ -7,24 +7,51 @@ module Xmax = {
     codes: array<int>,
     runLength: int,
   }
+
+  type xmaxError = InvalidRunLength | InvalidIndex
+
   let codes = t => t.codes
   let runLength = t => t.runLength
 
-  // is x sum of 2 elem in xs
-  let isSumOf = (xs, x) => true
+  // is sum sum of x with one of xs
+  // if so, return array of matches
+  let isSumofWith = (xs, x, ~sum) => {
+    xs->Array.keep(a => {
+      a != x && a + x == sum
+    })
+  }
+
+  // is sum sum of 2 elem in xs
+  let isSumOf = (xs, sum) => {
+    xs->Array.keep(a => {
+      xs->isSumofWith(a, ~sum)->Array.size > 0
+    })
+  }
 
   // is code at index i (0-base) a valid code
   let isCodeValid = (t, i) => {
     let lastSet = t->codes->Array.slice(~offset=i - t->runLength, ~len=t->runLength)
-    let code = t.codes->Array.getExn(i)
-    code->log
-
-    lastSet->log
-
-    isSumOf(lastSet, code)
+    let c = t.codes->Array.get(i)
+    switch c {
+    | Some(code) => {
+        code->log
+        lastSet->log
+        isSumOf(lastSet, code)->Ok
+      }
+    | None => Error(InvalidIndex)
+    }
   }
 
-  let make = (codes, runLength) => {codes: codes, runLength: runLength}
+  type makeResult = Result.t<t, xmaxError>
+
+  let make = (codes, runLength): makeResult => {
+    runLength->log
+    codes->Array.size->log
+    switch codes->Array.size > runLength {
+    | true => Ok({codes: codes, runLength: runLength})
+    | false => Error(InvalidRunLength)
+    }
+  }
 }
 
 let parse = data =>
@@ -36,8 +63,8 @@ let parse = data =>
 
 let solvePart1 = data => {
   let xmax = Xmax.make(data->parse, 5)
-  xmax->log
-  xmax->Xmax.isCodeValid(5)
+  xmax->Result.getExn->log
+  xmax->Result.getExn->Xmax.isCodeValid(14)->Result.getExn
 }
 
 let solvePart2 = data => {
