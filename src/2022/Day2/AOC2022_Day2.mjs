e2f2c56415f78074b83867426d78826f26abfd3b
Danny Siu <danny.siu@gmail.com>
2022-12-05 17:57:06 +0800

    2022 Day 2 - Part 1 WIP

--- src/2022/Day2/AOC2022_Day2.mjs
+++ src/2022/Day2/AOC2022_Day2.mjs
@@ -7,6 +7,67 @@ function log(prim) {
   console.log(prim);
 }
 
+function player2Result(round) {
+  switch (round._0) {
+    case /* Rock */0 :
+        switch (round._1) {
+          case /* Rock */0 :
+              return /* Draw */1;
+          case /* Paper */1 :
+              return /* Win */2;
+          case /* Scissors */2 :
+              return /* Loss */0;
+          
+        }
+    case /* Paper */1 :
+        switch (round._1) {
+          case /* Rock */0 :
+              return /* Loss */0;
+          case /* Paper */1 :
+              return /* Draw */1;
+          case /* Scissors */2 :
+              return /* Win */2;
+          
+        }
+    case /* Scissors */2 :
+        switch (round._1) {
+          case /* Rock */0 :
+              return /* Win */2;
+          case /* Paper */1 :
+              return /* Loss */0;
+          case /* Scissors */2 :
+              return /* Draw */1;
+          
+        }
+    
+  }
+}
+
+function scoreShape(s) {
+  return 1 + (s + 1 | 0) | 0;
+}
+
+function scoreResult(r) {
+  var tmp;
+  switch (r) {
+    case /* Loss */0 :
+        tmp = 0;
+        break;
+    case /* Draw */1 :
+        tmp = 3;
+        break;
+    case /* Win */2 :
+        tmp = 6;
+        break;
+    
+  }
+  return Math.imul(3, tmp);
+}
+
+function scoreRound(r) {
+  return scoreResult(player2Result(r)) + scoreShape(r._1) | 0;
+}
+
 function parse(data) {
   return Belt_Array.map(Utils$AdventOfCode.splitNewline(data), (function (prim) {
                 return prim.trim();
@@ -23,6 +84,10 @@ function solvePart2(data) {
 
 export {
   log ,
+  player2Result ,
+  scoreShape ,
+  scoreResult ,
+  scoreRound ,
   parse ,
   solvePart1 ,
   solvePart2 ,
--- src/2022/Day2/AOC2022_Day2.res
+++ src/2022/Day2/AOC2022_Day2.res
@@ -2,6 +2,46 @@ open Belt
 open Utils
 let log = Js.Console.log
 
+type shape = Rock | Paper | Scissors
+type result = Loss | Draw | Win
+type round = Round(shape, shape)
+
+let player2Result: round => result = round => {
+  switch round {
+  | Round(Rock, Paper) => Win
+  | Round(Paper, Scissors) => Win
+  | Round(Scissors, Rock) => Win
+  | Round(Rock, Rock)
+  | Round(Paper, Paper)
+  | Round(Scissors, Scissors) =>
+    Draw
+  | _ => Loss
+  }
+}
+
+let scoreShape: shape => int = s => {
+  1 +
+  switch s {
+  | Rock => 1
+  | Paper => 2
+  | Scissors => 3
+  }
+}
+
+let scoreResult: result => int = r => {
+  3 *
+  switch r {
+  | Loss => 0
+  | Draw => 3
+  | Win => 6
+  }
+}
+
+let scoreRound: round => int = r => {
+  let Round(_, p2) = r
+  scoreResult(player2Result(r)) + scoreShape(p2)
+}
+
 let parse = data => data->splitNewline->Array.map(Js.String2.trim)
 
 let solvePart1 = data => {
