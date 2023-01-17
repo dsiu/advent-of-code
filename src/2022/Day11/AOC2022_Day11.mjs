18c2dae43be152b9ad3b0c1a0715db8fbef6f60d
Danny Siu <danny.siu@gmail.com>
2023-01-18 01:48:53 +0800

    2022 Day 11 - Part 1 WIP - Monadic Parser with Relude-Parse DONE!

--- src/2022/Day11/AOC2022_Day11.mjs
+++ src/2022/Day11/AOC2022_Day11.mjs
@@ -3,57 +3,109 @@
 import * as Curry from "rescript/lib/es6/curry.js";
 import * as Relude_Int from "relude/src/Relude_Int.mjs";
 import * as Relude_Map from "relude/src/Relude_Map.mjs";
-import * as Relude_Array from "relude/src/Relude_Array.mjs";
-import * as Relude_Option from "relude/src/Relude_Option.mjs";
-import * as Relude_String from "relude/src/Relude_String.mjs";
+import * as Relude_List from "relude/src/Relude_List.mjs";
+import * as Relude_Result from "relude/src/Relude_Result.mjs";
 import * as Relude_Function from "relude/src/Relude_Function.mjs";
+import * as ReludeParse_Parser from "relude-parse/src/ReludeParse_Parser.mjs";
 
 function log(prim) {
   console.log(prim);
+}
+
+function log2(prim0, prim1) {
+  console.log(prim0, prim1);
+}
+
+function makeLiteral(i) {
+  return /* Literal */{
+          _0: i
+        };
+}
+
+function showExpression(param) {
+  var operand = param._1;
+  return (
+          param._0 ? " * " : " + "
+        ) + (
+          operand ? String(operand._0) : "old"
+        );
+}
+
+function logExp(param) {
+  return Relude_Function.compose(log, showExpression, param);
+}
+
+function makeExpression(op, operand) {
+  return /* Expression */{
+          _0: op,
+          _1: operand
+        };
 }
 
 var include = Relude_Map.WithOrd(Relude_Int.Ord);
 
 var IntMap = include;
 
-function parseEachMonkey(s) {
-  var __x = Relude_String.splitArray("\n", s);
-  var s$1 = Curry._2(Relude_Array.map, Relude_String.trim, __x);
-  var parseMId = function (s) {
-    return Relude_Option.bind(s, (function (x) {
-                  return Relude_Option.bind(Relude_Array.at(1, Relude_String.splitArray(" ", x)), (function (x) {
-                                return Relude_Option.bind(Relude_Array.at(0, Relude_String.splitArray(":", x)), Relude_String.toInt);
-                              }));
-                }));
-  };
-  var parseStarting = function (s) {
-    return Relude_Option.bind(s, (function (x) {
-                  return Relude_Option.bind(Relude_Array.at(1, Relude_String.splitArray("Starting items: ", x)), (function (x) {
-                                var __x = Relude_String.splitArray(",", x);
-                                return Relude_Option.pure(Curry._2(Relude_Array.map, (function (x) {
-                                                  return Relude_String.toInt(Relude_String.trim(x));
-                                                }), __x));
-                              }));
-                }));
-  };
-  var mId = parseMId(Relude_Array.at(0, s$1));
-  var holding = parseStarting(Relude_Array.at(1, s$1));
+var mIdP = Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("Monkey "), ReludeParse_Parser.anyInt), ReludeParse_Parser.str(":")), ReludeParse_Parser.eol);
+
+var startingP = Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("  Starting items: "), ReludeParse_Parser.sepBy(ReludeParse_Parser.str(", "), ReludeParse_Parser.anyInt)), ReludeParse_Parser.eol);
+
+var opP = Curry._2(ReludeParse_Parser.Infix.$less$pipe$great, Curry._2(ReludeParse_Parser.Infix.$less$, /* Plus */0, ReludeParse_Parser.str("+")), Curry._2(ReludeParse_Parser.Infix.$less$, /* Times */1, ReludeParse_Parser.str("*")));
+
+var operandP = Curry._2(ReludeParse_Parser.Infix.$less$pipe$great, Curry._2(ReludeParse_Parser.Infix.$less$$great, makeLiteral, ReludeParse_Parser.anyInt), Curry._2(ReludeParse_Parser.Infix.$less$, /* Old */0, ReludeParse_Parser.str("old")));
+
+var expressionP = Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$$great, makeExpression, Curry._2(ReludeParse_Parser.Infix.$less$star, opP, ReludeParse_Parser.str(" "))), operandP);
+
+var operatorP = Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("  Operation: new = old "), expressionP), ReludeParse_Parser.eol);
+
+var testP = Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("  Test: divisible by "), ReludeParse_Parser.anyInt), ReludeParse_Parser.eol);
+
+var trueTargetP = Curry._2(ReludeParse_Parser.Infix.$less$star, Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("    If true: throw to monkey "), ReludeParse_Parser.anyInt), ReludeParse_Parser.eol);
+
+var falseTargetP = Curry._2(ReludeParse_Parser.Infix.$star$great, ReludeParse_Parser.str("    If false: throw to monkey "), ReludeParse_Parser.anyInt);
+
+function mkMonkeyPair(mId, holding, operation, test, trueTarget, falseTarget) {
   return [
           mId,
-          holding
+          Relude_List.toArray(holding),
+          showExpression(operation),
+          test,
+          trueTarget,
+          falseTarget
         ];
 }
 
-function parse(data) {
-  var __x = Relude_String.splitArray("\n\n", data);
-  return Curry._2(Relude_Array.map, (function (param) {
-                return Relude_Function.compose(parseEachMonkey, Relude_String.trim, param);
-              }), __x);
+var monkeyP = Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$star$great, Curry._2(ReludeParse_Parser.Infix.$less$$great, mkMonkeyPair, mIdP), startingP), operatorP), testP), trueTargetP), falseTargetP);
+
+function makeMonkeyMaps(monkeys) {
+  return monkeys;
 }
 
+var monkeysP = Curry._2(ReludeParse_Parser.Infix.$less$$great, makeMonkeyMaps, ReludeParse_Parser.sepBy(Curry._2(ReludeParse_Parser.Infix.$less$star, ReludeParse_Parser.eol, ReludeParse_Parser.eol), monkeyP));
+
+function parse(s) {
+  return ReludeParse_Parser.runParser(s, monkeysP);
+}
+
+var MonkeyParser = {
+  P: undefined,
+  mIdP: mIdP,
+  startingP: startingP,
+  operatorP: operatorP,
+  testP: testP,
+  trueTargetP: trueTargetP,
+  falseTargetP: falseTargetP,
+  monkeyP: monkeyP,
+  monkeysP: monkeysP,
+  parse: parse
+};
+
 function solvePart1(data) {
-  var prim = parse(data);
-  console.log(prim);
+  var result = ReludeParse_Parser.runParser(data, monkeysP);
+  Relude_Result.tap((function (x) {
+          var prim = Relude_List.toArray(x);
+          console.log(prim);
+        }), result);
   return 1;
 }
 
@@ -67,17 +119,30 @@ var S;
 
 var A;
 
+var L;
+
 var O;
+
+var R;
+
+var F;
 
 export {
   log ,
+  log2 ,
   compose ,
   S ,
   A ,
+  L ,
   O ,
+  R ,
+  F ,
+  makeLiteral ,
+  showExpression ,
+  logExp ,
+  makeExpression ,
   IntMap ,
-  parseEachMonkey ,
-  parse ,
+  MonkeyParser ,
   solvePart1 ,
   solvePart2 ,
 }
--- src/2022/Day11/AOC2022_Day11.res
+++ src/2022/Day11/AOC2022_Day11.res
@@ -1,5 +1,6 @@
 // let's try to use Relude
 let log = Js.Console.log
+let log2 = Js.Console.log2
 
 open Relude
 //open Utils
@@ -8,11 +9,30 @@ open Relude
 let compose = Function.compose
 module S = String
 module A = Array
+module L = List
 module O = Option
+module R = Result
+module F = Function
 
 type operator = Plus | Times
 type operand = Literal(int) | Old
+let makeLiteral = i => Literal(i)
 type expression = Expression(operator, operand)
+
+let showExpression = (Expression(op, operand)) => {
+  switch op {
+  | Plus => " + "
+  | Times => " * "
+  } ++
+  switch operand {
+  | Literal(i) => i->string_of_int
+  | Old => "old"
+  }
+}
+let logExp = compose(log, showExpression)
+
+let makeExpression = (op, operand) => Expression(op, operand)
+
 type monkeyCode = MonkeyCode({operation: expression, test: int, trueTarget: int, falseTarget: int})
 
 module IntMap = {
@@ -26,46 +46,85 @@ type monkeyHolds = IntMap.t<array<int>> // state
 type monkeyLog = MonkeyLog(int, int) // writer -- monkey ID, number of items handled this round
 // type monkeyHandler = RWS MonkeyDescription [MonkeyLog] monkeyHolds
 
-let parseEachMonkey = s => {
-  let s = s->S.splitArray(~delimiter="\n", _)->A.map(S.trim, _)
+module MonkeyParser = {
+  module P = ReludeParse.Parser
+  open P.Infix
 
-  let parseMId = s => {
-    s->O.bind(x => {
-      x
-      ->S.splitArray(~delimiter=" ", _)
-      ->A.at(1, _)
-      ->O.bind(x => {
-        x
-        ->S.splitArray(~delimiter=":", _)
-        ->A.at(0, _)
-        ->O.bind({
-          x => x->S.toInt
-        })
-      })
-    })
+  // Monkey 0:
+  //  Starting items: 79, 98
+  //  Operation: new = old * 19
+  //  Test: divisible by 23
+  //    If true: throw to monkey 2
+  //    If false: throw to monkey 3
+
+  let mIdP = P.str("Monkey ")->\"*>"(P.anyInt)->\"<*"(P.str(":"))->\"<*"(P.eol)
+  // let _ = mIdP->P.runParser("Monkey 123:\n", _)->R.tap(log, _)
+
+  let startingP =
+    P.str("  Starting items: ")->\"*>"(P.anyInt->P.sepBy(P.str(", "), _))->\"<*"(P.eol)
+  // let _ = startingP->P.runParser("  Starting items: 79, 98, 100\n", _)->R.tap(log, _)
+
+  let operatorP = {
+    let expressionP = {
+      let opP = Plus->\"<$"(P.str("+"))->\"<|>"(Times->\"<$"(P.str("*")))
+      let operandP = makeLiteral->\"<$>"(P.anyInt)->\"<|>"(Old->\"<$"(P.str("old")))
+      makeExpression->\"<$>"(opP->\"<*"(P.str(" ")))->\"<*>"(operandP)
+    }
+
+    P.str("  Operation: new = old ")->\"*>"(expressionP)->\"<*"(P.eol)
+  }
+  //  let _ = operatorP->P.runParser("  Operation: new = old + 19\n", _)->R.tap( logExp, _)
+  //  let _ = operatorP->P.runParser("  Operation: new = old * 20\n", _)->R.tap( logExp, _)
+  //  let _ = operatorP->P.runParser("  Operation: new = old + old\n", _)->R.tap(logExp, _)
+  //  let _ = operatorP->P.runParser("  Operation: new = old * old\n", _)->R.tap(logExp, _)
+
+  let testP = P.str("  Test: divisible by ")->\"*>"(P.anyInt)->\"<*"(P.eol)
+  // let _ = testP->P.runParser("  Test: divisible by 23\n", _)->R.tap(log, _)
+  let trueTargetP = P.str("    If true: throw to monkey ")->\"*>"(P.anyInt)->\"<*"(P.eol)
+  //  let _ = trueTargetP->P.runParser("    If true: throw to monkey 12\n", _)->R.tap(log, _)
+  let falseTargetP = P.str("    If false: throw to monkey ")->\"*>"(P.anyInt)
+  //  let _ = falseTargetP->P.runParser("    If false: throw to monkey 33", _)->R.tap(log, _)
+
+  let monkeyP = {
+    let mkMonkeyPair = (mId, holding, operation, test, trueTarget, falseTarget) => {
+      //      ((mId, MonkeyCode({operation, test, trueTarget, falseTarget})), (mId, holding))
+      (mId, holding->L.toArray, operation->showExpression, test, trueTarget, falseTarget)
+    }
+
+    mkMonkeyPair
+    ->\"<$>"(mIdP)
+    ->\"<*>"(startingP)
+    ->\"<*>"(operatorP)
+    ->\"<*>"(testP)
+    ->\"<*>"(trueTargetP)
+    ->\"<*>"(falseTargetP)
+  }
+  //  let _ =
+  //    monkeyP
+  //    ->P.runParser(
+  //      "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n",
+  //      _,
+  //    )
+  //    ->R.tap(log, _)
+
+  let monkeysP = {
+    let makeMonkeyMaps = monkeys => {
+      //      monkeys->log2("monkeys")
+      monkeys
+    }
+    makeMonkeyMaps->\"<$>"(monkeyP->P.sepBy(P.eol->\"<*"(P.eol), _))
+    //    monkeyP->P.sepBy(P.eol->\"<*"(P.eol), _)
   }
 
-  let parseStarting = s => {
-    s->O.bind(x => {
-      x
-      ->S.splitArray(~delimiter="Starting items: ", _)
-      ->A.at(1, _)
-      ->O.bind(x => {
-        x->S.splitArray(~delimiter=",", _)->A.map({x => x->S.trim->S.toInt}, _)->O.pure
-      })
-    })
-  }
-
-  let mId = A.at(0, s)->parseMId
-  let holding = A.at(1, s)->parseStarting
-  (mId, holding)
+  let parse = s => monkeysP->P.runParser(s, _)
 }
 
-let parse = data =>
-  data->S.splitArray(~delimiter="\n\n", _)->A.map(compose(parseEachMonkey, S.trim), _)
+//let parse = data =>
+//  data->S.splitArray(~delimiter="\n\n", _)->A.map(compose(parseEachMonkey, S.trim), _)
 
 let solvePart1 = data => {
-  data->parse->log
+  let result = data->MonkeyParser.parse
+  let _ = result->R.tap(x => x->L.toArray->log, _)
   1
 }
 
